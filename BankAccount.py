ef367c7708395783881719f34ea82d601345c33e
naif-alharbi22 <naif.n1158@gmail.com>
2024-06-26 07:43:51 +0300

    solved : lab calss done

--- BankAccount.py
+++ BankAccount.py
@@ -1,19 +1,19 @@
 import random
 class BankAccount:
     def __init__(self, account_holder , initial_balance = 0 ) -> None:
-        card=[]
+        self.card=card=[]
         while True:
             number="".join(random.choices('123456789' , k=10))
             if not number in card:
-                card.append(number)
                 break
         account_number=number
         self.account_number=account_number
         self.account_holder=account_holder
         self.initial_balance=initial_balance
+        card.append(account_holder , )
     def deposit(self, pay:int)-> int:
         self.initial_balance+=pay
-        return self.initial_balance
+        return f"Your account is now {self.initial_balance} riyals"
     def withdraw(self , pay:int):
         if self.initial_balance <= pay:
             return f"The balance is insufficient. Your balance is {self.initial_balance} riyals"
@@ -21,8 +21,10 @@ class BankAccount:
             self.initial_balance=self.initial_balance-pay
             return f"The transaction was completed successfully, your balance is now {self.initial_balance} riyals"
     def get_balance(self):
-        return self.initial_balance
+        return f"The amount in your account is {self.initial_balance} riyals"
     def get_account_holder(self):
-        return self.account_holder
+        return f"The account holder's name is {self.account_holder}"
     def get_account_number(self):
-        return self.account_number
+        return f"{self.account_holder} account number is {self.account_number}"
+    
+    
--- main.py
+++ main.py
@@ -1,8 +1,20 @@
 from BankAccount import BankAccount
 
+
+
 account=BankAccount("naif", 10)
 account1=BankAccount("ahmad", 200)
 account2=BankAccount("salem", 1000)
 
-print(account)
-print(account1)
+print(account.deposit(40))
+print("-"*5)
+print(account1.withdraw(20))
+print("-"*5)
+print(account1.withdraw(300))
+print("-"*5)
+print(account2.get_balance())
+print("-"*5)
+print(account2.get_account_holder())
+print("-"*5)
+print(account2.get_account_number())
+
