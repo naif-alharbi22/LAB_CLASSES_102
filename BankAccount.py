2c76f9e3311b6e4c447a0e63634be13226ac133e
naif-alharbi22 <naif.n1158@gmail.com>
2024-07-06 12:10:12 +0300

    The problem has been resolved

--- BankAccount.py
+++ BankAccount.py
@@ -1,30 +1,38 @@
 import random
 class BankAccount:
-    def __init__(self, account_holder , initial_balance = 0 ) -> None:
-        self.card=card=[]
+
+    _cards = []
+
+    def __init__(self, account_holder , initial_balance = 0 ):
+      self._account_number = self._generate_unique_account_number()
+      self._account_holder = account_holder
+      self._initial_balance = initial_balance
+
+    def _generate_unique_account_number(self):
         while True:
-            number="".join(random.choices('123456789' , k=10))
-            if not number in card:
-                break
-        account_number=number
-        self.account_number=account_number
-        self.account_holder=account_holder
-        self.initial_balance=initial_balance
-        card.append(account_holder , )
+            number = "".join(random.choices('123456789', k=10))
+            if number not in BankAccount._cards:
+                BankAccount._cards.append(number)
+                return number
+            
     def deposit(self, pay:int)-> int:
-        self.initial_balance+=pay
-        return f"Your account is now {self.initial_balance} riyals"
+        self._initial_balance+=pay
+        return f"Your account is now {self._initial_balance} riyals"
+    
     def withdraw(self , pay:int):
-        if self.initial_balance <= pay:
-            return f"The balance is insufficient. Your balance is {self.initial_balance} riyals"
+        if self._initial_balance <= pay:
+            return f"The balance is insufficient. Your balance is {self._initial_balance} riyals"
         else:
-            self.initial_balance=self.initial_balance-pay
-            return f"The transaction was completed successfully, your balance is now {self.initial_balance} riyals"
+            self._initial_balance=self._initial_balance-pay
+            return f"The transaction was completed successfully, your balance is now {self._initial_balance} riyals"
+        
     def get_balance(self):
-        return f"The amount in your account is {self.initial_balance} riyals"
+        return f"The amount in your account is {self._initial_balance} riyals"
+    
     def get_account_holder(self):
-        return f"The account holder's name is {self.account_holder}"
+        return f"The account holder's name is {self._account_holder}"
+    
     def get_account_number(self):
-        return f"{self.account_holder} account number is {self.account_number}"
+        return f"{self._account_holder} account number is {self._account_number}"
     
     
